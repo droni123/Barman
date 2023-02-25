10ba7494de2fa61f7aa5013f659bdf5510dca68b
Idel <idlch.perte@gmail.com>
2023-02-24 21:19:25 -0600

    Initial Commit

---
+++ Barman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Barman
+//
+//  Created by De la Cruz Hernandez on 24/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
